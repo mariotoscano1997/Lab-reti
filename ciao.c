683c7d7e1fe8843d3db2bdbd11cae3199d48f5c3
mariotoscano1997 <toscanomario97@gmail.com>
2018-06-25 04:32:31 +0200

    Aggiunto reciver

--- ciao.c
+++ ciao.c
@@ -28,13 +28,33 @@ int main( int argc , char** argv){
 		if(bind(sockfd,(struct sockaddr*)&locale, sizeof(locale))<0){
 			printf("errore nel bind\n");
 			return 3;
-		}
+			}
 		while(1){
 			printf("server in attesa\n");
 			recvfrom(sockfd, msg, 81, 0, (struct sockaddr *)&sender, &len);
+			}
 		}
 	}
-}
-else {
+	else {
+		int sockfd, n;
+		extern int errno;
+		struct sockaddr_in locale, reciver;
+		socklen_t len= sizeof(sender);
+		char msg[81];
 
+		if(argc<3) { printf("inserire indirizzo porta\n"); return 0;}
+		if((sockfd=socket(AF_INET, SOCK_DGRAM,0))<0){
+			printf("errore nel socket\n");
+			return 2;
+		}
+		memset(&reciver,0,sizeof(reciver));
+		reciver.sin_family= AF_INET;
+		reciver.sin_addr.s_addr=inet_addr(argv[1]);
+		reciver.sin_port=htons(atoi(argv[2]));
+		
+		while(fgets(msg,81,stdin)!=NULL){
+			printf("invio dati\n");
+			sendto(sockfd, msg, 81, 0, (struct sockaddr *)&sender, len);
+		}
+	}
 }
